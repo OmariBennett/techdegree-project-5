044b487b065a9d3a19afae96a013dfbbfbbb7372
Omari <omari.bennett@gmail.com>
2018-05-05 09:13:02 -0400

    Add the abbreviation state to the modal window display

--- js/app.js
+++ js/app.js
@@ -4,26 +4,69 @@ function titleCase(str) {
   }).join(' ');
 }
 
+let statesList = [
+    ["AL", "Alabama"],
+    ["AK", "Alaska"],
+    ["AZ", "Arizona"],
+    ["AR", "Arkansas" ],
+    ["CA", "California"],
+    ["CO", "Colorado"],
+    ["CT", "Connecticut"],
+    ["DE", "Delaware"],
+    ["FL", "Florida"],
+    ["GA", "Georgia"],
+    ["HI", "Hawaii"],
+    ["ID", "Idaho"],
+    ["IL", "Illinois"],
+    ["IN", "Indiana"],
+    ["IA", "Iowa"],
+    ["KS", "Kansas"],
+    ["KY", "Kentuck},"],
+    ["LA", "Louisiana"],
+    ["ME", "Maine"],
+    ["MD", "Maryland"],
+    ["MA", "Massachusetts"],
+    ["MI", "Michigan"],
+    ["MN", "Minnesota"],
+    ["MS", "Mississippi"],
+    ["MO", "Missouri"],
+    ["MT", "Montana"],
+    ["NE", "Nebraska"],
+    ["NV", "Nevada"],
+    ["NH", "New Hampshire"],
+    ["NJ", "New Jersey"],
+    ["NM", "New Mexico"],
+    ["NY", "New York"],
+    ["NC", "North Carol},na"],
+    ["ND", "North Dakota"],
+    ["OH", "Ohio"],
+    ["OK", "Oklahoma"],
+    ["OR", "Oregon"],
+    ["PA", "Pennsylvania"],
+    ["RI", "Rhode Island"],
+    ["SC", "South Carolina"],
+    ["SD", "South Dakota"],
+    ["TN", "Tennessee"],
+    ["TX", "Texas"],
+    ["UT", "Utah"],
+    ["VT", "Vermont"],
+    ["VA", " Virginia"],
+    ["WA","Washington"],
+    ["WV", "West Virginia"],
+    ["WI", "Wisconsin"],
+    ["WY", "Wyoming"]
+]
+
 $.getJSON({
   url: 'https://randomuser.me/api/?results=12&nat=us',
   dataType: 'json',
   success: function(data) {
 
-    // let employeeHTML = '<ul>';
-    // $.each( data.results, function( index, employee) {
-    // 	employeeHTML += `<li class="employeeCard">`;
-    // 	employeeHTML += `<img src="${employee.picture.large}" class="employeeImg">`;
-    // 	employeeHTML += `<p class="f_Name">${titleCase(employee.name.first)} ${titleCase(employee.name.last)}</p>`;
-    // 	employeeHTML += `<p class="email">${employee.email}</p>`;
-    // 	employeeHTML += `<p class="state">${titleCase(employee.location.city)}</p></li>`;
-    // });
-    // employeeHTML += `<li class="employeeCard">`;;
-    // $('#employeeList').html(employeeHTML);
-
     const ul = document.querySelector('ul');
     let employeeHTML = '',
         employee = data.results;
 
+    //Loop through ajax data and set up HTML tags
     for(let i = 0; i < employee.length; i++) {
         employeeHTML += `<li class="employeeCard">`;
         employeeHTML += `<img src="${employee[i].picture.large}" class="employeeImg">`;
@@ -34,13 +77,22 @@ $.getJSON({
 
     } //End of the for loop
 
+    //append the string to the page
     $(ul).html(employeeHTML);
 
+    //Set event listener to each Modal window displays on the page
     for(let i = 0; i < ul.children.length; i++) {
-        ul.children[i].addEventListener('click', (e)=> {
-            console.log(`Card info!`);
-            console.log(i);
-            
+        ul.children[i].addEventListener('click', ()=> {
+
+            //Change employee state to it's abbreviation state i.e. Maryland to MD
+            function abbreviationState(state) {
+                for(let j = 0; j < statesList.length; j++) {
+                    if (state === statesList[j][1].toLowerCase()) {
+                        return statesList[j][0];
+                    }
+                }
+            }
+
             //Create HTML Elements
             const body = document.querySelector('body'),
                   h1 = document.querySelector('h1'),
@@ -56,9 +108,9 @@ $.getJSON({
             employeeInfo += `<p class="cardStyle moreInfo">${data.results[i].email}</p>`;
             employeeInfo += `<p class="cardStyle moreInfo">${titleCase(data.results[i].location.city)}</p>`;
 
-            employeeInfo += `<hr><p class="cardStyle moreInfo">${data.results[i].phone}</p>`;
-            employeeInfo += `<p class="cardStyle moreInfo">${titleCase(data.results[i].location.street)}, ${titleCase(data.results[i].location.state)} ${data.results[i].location.postcode}</p>`;
-            employeeInfo += `<p class="cardStyle moreInfo">Birthday: ${data.results[i].dob}</p>`;
+            employeeInfo += `<hr><p class="cardStyle moreInfo">${data.results[i].cell}</p>`;
+            employeeInfo += `<p class="cardStyle moreInfo">${titleCase(data.results[i].location.street)}, ${abbreviationState(data.results[i].location.state)} ${data.results[i].location.postcode}</p>`;
+            employeeInfo += `<p class="cardStyle moreInfo">Birthday: ${data.results[i].dob.replace(/\d\d(\d\d)-(\d\d)-(\d\d).*/, '$2/$3/$1')}</p>`;
             employeeInfo += `</div>`;
 
             //Set attruibutes
@@ -67,66 +119,16 @@ $.getJSON({
             //Append HTML to the page
             $(overlay).html(employeeInfo);
             body.insertBefore(overlay, h1);
+
+            //Set up exit "X" button
+            $('.btn').on('click', ()=> {
+                $('.overlay').remove();
+            });
+            
         });
-    }
-    // $('.employeeCard').on('click', ()=> {
-    //     console.log(`Card info!`);
-        // //variables
-        // const xBtn = document.createElement('p'),
-        //       div = document.querySelector('.employeeCard'),
-        //       img = document.querySelector('.employeeImg'),
-        //       fullName = document.querySelector('.f_Name'),
-        //       email = img.nextSibling;
-        //       city = img.nextSibling.nextSibling;
-        //       hr = document.querySelector('hr');
-        //       phone = hr.nextSibling;
-        //       street = hr.nextSibling.nextSibling;
-        //       dob = hr.nextSibling.nextSibling.nextSibling;
-
-        // //Setting attruibutes
-        // xBtn.innerHTML = 'X';
-        // xBtn.setAttribute('class', 'btn');
-        // img.setAttribute('class', 'employeeImg pic');
-        // fullName.setAttribute('class', 'f_Name moreInfo');
-        // email.setAttribute('class', 'cardStyle moreInfo');
-        // city.setAttribute('class', 'cardStyle moreInfo');
-        // hr.setAttribute('class', 'enable');
-        // phone.setAttribute('class', 'cardStyle moreInfo');
-        // street.setAttribute('class', 'cardStyle moreInfo');
-        // dob.setAttribute('class', 'cardStyle moreInfo');
-
-        // $('ul li .employeeCard').append(xBtn);
-    // }); //End of the on method
-
-    // $('#employeeList ul').on('click', ()=> {
-    //     console.log(`Card info!`);
-    //     console.log( $(this).index())
-    // 	// let employeeInfo = `<img src="${employee.picture.large}" class="employeeImg moreInfo">`;
-
-    // 	// // employeeInfo += `<img src="${employee.picture.large}" class="employeeImg moreInfo">`;
-    // 	// employeeInfo += `<p class="f_Name"${titleCase(employee.name.first)} ${titleCase(employee.name.last)}</p>`;
-    // 	// employeeInfo += `<p class="cardStyle">${employee.email}</p>`;
-    // 	// employeeInfo += `<p class="cardStyle"${titleCase(employee.location.city)}</p><hr>`;
-
-    // 	// employeeInfo += `<p class="cardStyle">${employee.phone}</p>`;
-    // 	// employeeInfo += `<p class="cardStyle">${titleCase(employee.location.street)}, ${titleCase(employee.location.state)} ${employee.location.postcode}</p>`;
-    // 	// employeeInfo += `<p class="cardStyle">Birthday: ${employee.location.dob}</p></div>`;
-
-    // 	// console.log(employeeInfo);
-    // 	/*
-    // 		<div class='lightBox'>
-    // 			<img class="employeeImg moreInfo">
-    // 			<p class="f_Name">Full Name</p>
-    // 			<p class="cardStyle">Email</p>
-    // 			<p class="cardStyle">State</p>
-    // 			<hr>
-    // 			<p class="cardStyle">Phone#</p>
-    // 			<p class="cardStyle">Address</p>
-    // 			<p class="cardStyle">B-day</p>
-    // 		</div>
-    // 	*/
-    // });
     
+    }//End of the for loop
+
     console.dir(data.results);
   }
 });
